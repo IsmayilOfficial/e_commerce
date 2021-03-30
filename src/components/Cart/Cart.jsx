6e059a78e806e340ed94d5a57b0e26013f65b4d3
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-03-30 15:34:52 +0400

    cart buttons functionalty

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import useStyles from './styles'
 import CartItem from './CartItem/CartItem';
 import {Link} from 'react-router-dom';
 
-const Cart = ({cart}) => {
+const Cart = ({cart,handleUpdateQty,handleRemoveFromCart, handleEmptyCart}) => {
     
     const classes = useStyles();
     const isEmpty = !cart.line_items.length ;
@@ -23,7 +23,7 @@ const Cart = ({cart}) => {
 
          {cart.line_items.map((item)=>(
              <Grid item xs={12} sm= {4} key = {item.id}>
-               <CartItem item = {item} />
+               <CartItem item = {item}  onUpdateCartQty = {handleUpdateQty} onRemoveFromCart ={handleRemoveFromCart}/>
 
               </Grid>   
            
@@ -36,7 +36,7 @@ const Cart = ({cart}) => {
                 <div>
 
 
-         <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty cart</Button>
+         <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty cart</Button>
           <Button className={classes.checkoutButton}  size="large" type="button" variant="contained" color="primary">Checkout</Button>
 
                 </div>
--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -4,8 +4,12 @@ import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@
 import useStyles from './styles';
 
 
-const CartItem = ({item}) => {
+
+const CartItem = ({item,onUpdateCartQty,onRemoveFromCart}) => {
     const classes = useStyles();
+    const handleUpdateCartQty = (lineItemId, newQuantity) => onUpdateCartQty(lineItemId, newQuantity);
+
+    const handleRemoveFromCart = (lineItemId) => onRemoveFromCart(lineItemId);
     return (
         <Card>
             <CardMedia image ={item.media.source} alt = {item.name} className = {classes.media}/>
@@ -18,12 +22,12 @@ const CartItem = ({item}) => {
             
             <CardActions className={classes.CardActions}> 
             <div className= {classes.buttons}>
-                <Button type= "button" size ="small">-</Button>
+                <Button type= "button" size ="small" onClick={() => handleUpdateCartQty(item.id, item.quantity -1)} >-</Button>
                 <Typography>{item.quantity}</Typography>
-                <Button type= "button" size ="small">+</Button>
+                <Button type= "button" size ="small" onClick={() => handleUpdateCartQty(item.id, item.quantity +1)}>+</Button>
 
             </div>
-            <Button variant="contained" type="button" color="secondary">Remove</Button>
+            <Button variant="contained" type="button" color="secondary" onClick={() => handleRemoveFromCart(item.id)}>Remove</Button>
 
             </CardActions>
         </Card>
